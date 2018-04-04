8f25ee1d3ed786db605fd2acbd943bc91ea823b7
joseandrestellespereira <joseandrestellespereira@gmail.com>
2018-04-04 00:46:43 -0300

    Correcao do FloatinLabel
    Nos campos para digitar era necessário excluir o texto

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -58,16 +58,16 @@ public final class R {
         public static final int center = 0x7f09002f;
         public static final int dark = 0x7f09003e;
         public static final int icon_only = 0x7f090067;
-        public static final int light = 0x7f090070;
-        public static final int none = 0x7f09008b;
-        public static final int normal = 0x7f09008c;
-        public static final int radio = 0x7f090098;
-        public static final int standard = 0x7f0900c1;
-        public static final int text = 0x7f0900c8;
-        public static final int text2 = 0x7f0900c9;
-        public static final int toolbar = 0x7f0900d4;
-        public static final int wide = 0x7f0900ea;
-        public static final int wrap_content = 0x7f0900ed;
+        public static final int light = 0x7f090077;
+        public static final int none = 0x7f090092;
+        public static final int normal = 0x7f090093;
+        public static final int radio = 0x7f09009f;
+        public static final int standard = 0x7f0900c8;
+        public static final int text = 0x7f0900cf;
+        public static final int text2 = 0x7f0900d0;
+        public static final int toolbar = 0x7f0900db;
+        public static final int wide = 0x7f0900f1;
+        public static final int wrap_content = 0x7f0900f4;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f0a0007;
